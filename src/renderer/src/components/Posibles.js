d31757042e78434fc90aa40f859fc26a3d02f5c2
Mikel <mikelgaztargmail.com>
2024-02-20 09:46:24 +0100

    holo

--- src/renderer/src/components/Posibles.js
+++ src/renderer/src/components/Posibles.js
@@ -55,9 +55,9 @@
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<Timer />);
 
+
+
 //Contador Manual
-
-
 
 // import { useState, useEffect } from "react";
 // import ReactDOM from "react-dom/client";
